a7cb2aeb7e241509074529c4fa5c1ed1c9c0ba29
swoo0514 <swoo0514lim@gmail.com>
2024-01-26 16:10:36 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Storage } = require('@google-cloud/storage');
+const sharp = require('sharp');
+
+exports.resizeAndUpload = (data, context) => {
+  const storage = new Storage();
+  const { bucket, name } = data;
+
+  if (!name.startsWith('original')) return 1;
+
+  const filename = name.split('/').at(-1);
+  const ext = filename.split('.').at(-1).toLowercase();
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
+
+  console.log('name', name, 'ext', ext);
+
+  const file = storage.bucket(bucket).file(name);
+  const readStream = file.createReadStream();
+
+  const newFile = storage.bucket(bucket).file(`thumb/${filename}`);
+  const writeStream = newFile.createWriteStream();
+
+  readStream
+    .pipe(sharp().resize(200, 200, { fit: 'inside' }).toFormat(requiredFormat))
+    .pipe(writeStream);
+  return new Promise((resolve, reject) => {
+    writeStream.on('finish', () => {
+      resolve(`thumb/${filename}`);
+    });
+    writeStream.on('error', reject);
+  });
+};
